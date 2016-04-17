172b122580d2bc62f2895c98c90405700f4ebe11
AmandaMcClintock <admccli@emory.edu>
2016-04-17 15:51:42 -0400

    color scheme

--- app/src/main/java/am/tabfragment/PagerAdapter.java
+++ app/src/main/java/am/tabfragment/PagerAdapter.java
@@ -3,8 +3,6 @@ package am.tabfragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
